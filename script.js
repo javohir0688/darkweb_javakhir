4938b5fdcb48ae8648f96aa421f4f4777d731dc7
javohir0688 <abdusattorovjavohir06@gmail.com>
2025-08-12 18:49:02 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const message = document.getElementById('message');
+
+openBtn.addEventListener('click', () => {
+  message.classList.remove('hidden');
+  message.classList.add('visible');
+  openBtn.style.display = 'none';
+  closeBtn.style.display = 'inline-block';
+});
+
+closeBtn.addEventListener('click', () => {
+  message.classList.remove('visible');
+  message.classList.add('hidden');
+  closeBtn.style.display = 'none';
+  openBtn.style.display = 'inline-block';
+});
